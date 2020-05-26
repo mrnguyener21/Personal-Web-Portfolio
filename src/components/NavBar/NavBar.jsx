e57db6b2b4b376dc854eb5f3211c8485370ab392
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-05-26 14:51:08 -0700

    added transitional underline for about me title and added a linkedin and github button to welcome section

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,23 +6,15 @@ import styles from './NavBar.module.scss'
 const NavBar = () => {
     return (
         <div className={styles.container}>
-            {/* <Link className={styles.component} to="/resume">Resume</Link>
-            <Link className={styles.component} to="/about">About</Link>
-            <Link className={styles.component} to="/work">Work</Link> */}
             <div className={styles.name}>
                 <h3>Victor Nguyen</h3>
             </div>
             <div className={styles.categoryContainer}>
-                {/* //currently these are place holders */}
                 <div className={styles.sections}>
                     <h3 className={styles.category}>About Me</h3>
                     <h3 className={styles.category}>Projects</h3>
                     <h3 className={styles.category}>Skills</h3>
                     <h3 className={styles.category}>Contact Me</h3>
-                </div>
-                <div className={styles.links}>
-                <h3 className={styles.category}>LinkedIn Logo</h3>
-                <h3 className={styles.category}>Github Logo</h3>
                 </div>
             </div>
         </div>
--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -5,10 +5,12 @@ const Welcome = () => {
     return (
         <div className={styles.container}>
             <div className={styles.introContainer}>
-            <h1 className={styles.intro}>Hi, I'm Victor, a front-end developer and the newest member of your team! Welcome to my portfolio and let me know if you see anything you like 👍</h1>
-            <div className={styles.buttonContainer}>
-            <button className={styles.button}>● RESUME ●</button>
-            </div>
+                <h1 className={styles.intro}>Hi, I'm Victor, a Self-Taught Front-End Developer and the newest member of your team! Welcome to my portfolio and let me know if you see anything you like 👍</h1>
+                <div className={styles.buttonContainer}>
+                    <button className={styles.button}>● RESUME ●</button>
+                    <button className={styles.button}>● LINKEDIN ●</button>
+                    <button className={styles.button}>● GITHUB ●</button>
+                </div>
             </div>
             <img className={styles.image} src={codeSnippet}/>
 
