c6f85807a2bca951ca56f415378c72b3de0efc1e
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-05-28 12:21:17 -0700

    added a darken color when hovering over buttons for UX

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,10 +4,8 @@ import styles from './Contact.module.scss'
 const Contact = () => {
     return (
         <div className={styles.container}>
-            <div>
-            <HomeButton/>
-            </div>
-                <h1 className={styles.title}>CONTACT ME</h1>
+            <HomeButton/>   
+            <h1 className={styles.title}>CONTACT ME</h1>
             <div className={styles.contentContainer}>
                 <h3 className={styles.intro}>Did you see anything you like? Did you want to find a way to reach me? Below are some ways you can 🤩</h3>
                 <div className={styles.contactContainer}>
