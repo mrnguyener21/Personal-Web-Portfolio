24b096d83b2fe54d129a6f1b1f904b13f1f6d697
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-05-28 22:11:29 -0700

    made navbar sticky

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { About, Project, NavBar, Contact, Welcome } from './components'
 
-import styles from './App.module.scss';
 const App = () => {
-    return (
-      <>
-        <a className = {styles.top} name='top'></a>
-        <NavBar/>
-        <Welcome />
-        <About/>
-        <Project/>
-        <Contact />
-      </>
-    );
+
+
+  return (
+  <>
+      <a name='top'></a>
+      <NavBar/>
+      <Welcome />
+      <About/>
+      <Project/>
+      <Contact />
+    </>
+  );
+  
 }
 
-export default App;
-
+  export default App;
+  
+  
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, {useRef} from 'react';
+import React from 'react';
 // import About from '../About/About';
 // import { render } from 'react-dom';
 import { Link, Element , Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
@@ -9,7 +9,7 @@ import styles from './NavBar.module.scss';
 const NavBar = () => {
 
     return (
-        <div className={styles.container}>
+        <div id='container' className={styles.container}>
             <div className={styles.name}>
                 <h3>Victor Nguyen</h3>
             </div>
@@ -17,13 +17,6 @@ const NavBar = () => {
                 <Link activeClass="active" className={styles.category} to="about" spy={true} smooth={true} duration={500} >About Me</Link>  
                 <Link activeClass="active" className={styles.category} to="project" spy={true} smooth={true} duration={500} >Projects</Link>  
                 <Link activeClass="active" className={styles.category} to="contact" spy={true} smooth={true} duration={500} >Contact Me</Link>  
-
-
-
-
-                {/* <h3 className={styles.category} >About Me</h3> */}
-                {/* <h3 className={styles.category}>Projects</h3>
-                <h3 className={styles.category}>Contact Me</h3> */}
             </div>
         </div>
     );
