abcc2fd938308dfa062b7c5ff7676c395722d075
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-06-11 15:28:10 -0700

    added link for resume

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Skills from './Skills/Skills';
 import Why_Me from './Why_Me/Why_Me';
+
 import styles from './About.module.scss';
 
 
--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 import codeSnippet from '../../image/codeSnippet.JPG'
 import styles from './Welcome.module.scss'
 const Welcome = () => {
-    const btnClick = (value) => value === 0 ? window.open("https://www.linkedin.com/in/victortnguyen/"):window.open("https://github.com/mrnguyener21")
-
+    const btnClick = (value) => {
+        if(value === 0) {
+            window.open("https://docs.google.com/document/d/1V2JcbedVLD-YL_ZJ2PzJ-20eDbRH_cBln5nrlhOixQo/view?usp=sharing")
+        } else if(value === 1){
+            window.open("https://www.linkedin.com/in/victortnguyen/")
+        } else if (value === 2){
+            window.open("https://github.com/mrnguyener21")
+        }
+    }
     return (
         <div id="welcome" className={styles.container}>
             <div className={styles.introContainer}>
@@ -11,9 +18,10 @@ const Welcome = () => {
                 <h2 className={styles.intro}> I'm a Self-Taught Front-End Developer and the newest member of your team! Welcome to my portfolio and let me know if you see anything you like.</h2>
                 <div className={styles.buttonAndImageContainer}>
                     <div className={styles.buttonContainer}>
-                        <button className={styles.button} id={styles.button1}>RESUME</button>
-                        <button className={styles.button} id={styles.button2} onClick={()=> btnClick(0)}>LINKEDIN</button>
-                        <button className={styles.button} id={styles.button3} onClick={()=> btnClick(1)}>GITHUB</button>
+                        {/* <a href='https://docs.google.com/document/d/1V2JcbedVLD-YL_ZJ2PzJ-20eDbRH_cBln5nrlhOixQo/view?usp=sharing'> */}
+                            <button className={styles.button} id={styles.button1} onClick={()=>btnClick(0)}>RESUME</button>
+                        <button className={styles.button} id={styles.button2} onClick={()=> btnClick(1)}>LINKEDIN</button>
+                        <button className={styles.button} id={styles.button3} onClick={()=> btnClick(2)}>GITHUB</button>
                     </div>
                     <img className={styles.image} src={codeSnippet} alt="code snippet" />   
                 </div>
