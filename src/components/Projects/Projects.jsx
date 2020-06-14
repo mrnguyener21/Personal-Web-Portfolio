648f7b8d6bbc20e2d9b208f0c6a60ade0617a507
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-06-14 09:58:12 -0700

    fixed minor typo

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -22,7 +22,7 @@ const projects = [
     {
         image: financialWebApp,
         name: 'FINANCIAL WEB APP',
-        description: 'A website containing different tools to help people with their financial needs. I created this web app as a solution to what I seen from my own usage of online financial tools. Even though the resources out there do work, I never saw a website containing multiple tools for the users to choose from and would have to go different websites for each of their financial needs.',
+        description: 'A web App containing different tools to help people with their financial needs. I created this web app as a solution to what I seen from my own usage of online financial tools. Even though the resources out there do work, I never saw a website containing multiple tools for the users to choose from and would have to go different websites for each of their financial needs.',
         technologies: 'Created With: HTML5 | SASS | JAVASCRIPT | REACT',
         sourceCode: 'https://github.com/mrnguyener21/FInancial_Tool_Web_App',
         liveDemo: 'https://vnfinancial.netlify.app/'
